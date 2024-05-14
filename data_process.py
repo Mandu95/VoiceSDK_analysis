e903a288fdab95847a0e1c009786cccb1f4f5cfc
mandu95@puzzle-ai.com <mandu95@puzzle-ai.com>
2024-05-14 10:28:36 +0900

    변경사항 저장 : DB 값 비어있을 때 예외처리 적용

--- data_process.py
+++ data_process.py
@@ -10,8 +10,29 @@ database_properties = nc.extract_properties(data) ## dic 형태에서 properties
 count = len(database_properties[0])
 status_fliter = []
 for A in range(count):
-    status_fliter.append(database_properties[0][A]['상태']['status']['name'])
+    
+    try:
+        if database_properties[0][A]['상태']['status']['name'] is not None:
+            status_fliter.append(database_properties[0][A]['상태']['status']['name'])
+            
+    except Exception as e:
+        print("Database를 다시 확인하시오, 데이터가 비어서 그럴거에요!")
 
 status_fliter = mandu_function.find_same_data(status_fliter)    
+
+goods_fliter = []
+for B in range(count):
     
+    try:
+        
+        if database_properties[0][B]['연관 제품']['select']['name'] is not None :
+
+            goods_fliter.append(database_properties[0][B]['연관 제품']['select']['name'])
+            
+    except Exception as e:
+        print("Database를 다시 확인하시오, 데이터가 비어서 그럴거에요!")
+        
+goods_fliter = mandu_function.find_same_data(goods_fliter)
+print(goods_fliter)
+
 
