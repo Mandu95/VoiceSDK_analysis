21cce918395851d7095e1dd3514d7e0be27cb508
mandu95@puzzle-ai.com <mandu95@puzzle-ai.com>
2024-05-14 10:34:23 +0900

    변경사항 저장 : streamlit 상단 레이아웃 이름 변경

--- mandu_dashboard.py
+++ mandu_dashboard.py
@@ -1,16 +1,19 @@
 import streamlit as st
+import data_process
+
 st.set_page_config(layout="wide")
 
 
 
 ##페이지 상단 영역
-st.subheader("Puzzle-AI VoiceSDK 대시보드")
+st.subheader("PuzzleAI's 사업부 대시보드")
 
 col9, col10 = st.columns([8, 2])
 st.subheader("Notion DB를 기준으로 분석한 자료입니다.:sunglasses:")
 
 # 탭메뉴 영역
-tab1, tab2 = st.tabs(["VoiceSDK 업체 현황", "VoiceSDK 파일"])
+tab1, tab2, tab3, tab4 = st.tabs(["VoiceENR", "VoiceSDK", "VoiceMARK","VoicDOC"])
 
 with tab1:
     col1, col2 = st.columns([5, 5])
+    st.write ("컨텐츠 삽입 예정")
