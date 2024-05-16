5579c1c191d75536b5e2e79362590d3fca2ead8b
mandu95@puzzle-ai.com <mandu95@puzzle-ai.com>
2024-05-16 16:44:16 +0900

    변경사항 : 임시 데이터 표시

--- mandu_dashboard.py
+++ mandu_dashboard.py
@@ -14,6 +14,57 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(
     ["VoiceEMR", "VoiceENR", "VoiceSDK", "VoiceMARK", "VoiceDOC"])
 
 with tab1:
-    col1, col2 = st.columns([5, 5])
-    st.metric(label="정식계약", value=14)
-    st.metric(label="데모계약", value=20)
+    col1, col2, col3 = st.columns([4, 3, 3])
+
+    with col1:
+        st.metric(label="고객", value=55)
+    with col2:
+        st.metric(label="정식계약", value=14)
+    with col3:
+        st.metric(label="데모계약", value=20)
+
+
+with tab2:
+
+    col4, col5, col6 = st.columns([4, 3, 3])
+
+    with col4:
+        st.metric(label="고객", value=4)
+    with col5:
+        st.metric(label="정식계약", value=14)
+    with col6:
+        st.metric(label="데모계약", value=20)
+
+
+with tab3:
+
+    col7, col8, col9 = st.columns([4, 3, 3])
+
+    with col7:
+        st.metric(label="고객", value=9)
+    with col8:
+        st.metric(label="정식계약", value=14)
+    with col9:
+        st.metric(label="데모계약", value=20)
+
+
+with tab4:
+
+    col10, col11, col12 = st.columns([4, 3, 3])
+
+    with col10:
+        st.metric(label="고객", value=1)
+    with col11:
+        st.metric(label="정식계약", value=14)
+    with col12:
+        st.metric(label="데모계약", value=20)
+
+with tab5:
+    col13, col14, col15 = st.columns([4, 3, 3])
+
+    with col13:
+        st.metric(label="고객", value=1)
+    with col14:
+        st.metric(label="정식계약", value=14)
+    with col15:
+        st.metric(label="데모계약", value=20)
