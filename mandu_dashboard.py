95f639be08be2abee3029b69f396ac91ec36bacc
mandu95@puzzle-ai.com <mandu95@puzzle-ai.com>
2024-05-16 11:26:23 +0900

    테스트 4

--- mandu_dashboard.py
+++ mandu_dashboard.py
@@ -1,4 +1,8 @@
+import data_process
 import streamlit as st
+
+contract = data_process.contract
+pro_contract = data_process.pre_contract
 st.set_page_config(layout="wide")
 
 
@@ -9,12 +13,10 @@ col9, col10 = st.columns([8, 2])
 st.subheader("Notion DB를 기준으로 분석한 자료입니다.:sunglasses:")
 
 # 탭메뉴 영역
-tab1, tab2, tab3, tab4 = st.tabs(
-    ["VoiceENR", "VoiceSDK", "VoiceMARK", "VoiceDOC"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(
+    ["VoiceEMR", "VoiceENR", "VoiceSDK", "VoiceMARK", "VoiceDOC"])
 
 with tab1:
     col1, col2 = st.columns([5, 5])
-    st.markdown('통계자료는 아래와 같습니다.')
-
     st.metric(label="정식계약", value=14)
     st.metric(label="데모계약", value=20)
