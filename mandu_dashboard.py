29cf01d6bd73b7d5f13a19e93156800cfa5a4077
mandu95@puzzle-ai.com <mandu95@puzzle-ai.com>
2024-05-16 11:00:08 +0900

    변경사항 저장 : 대시보드 상에 테스트 출력

--- mandu_dashboard.py
+++ mandu_dashboard.py
@@ -1,19 +1,22 @@
 import streamlit as st
-
+import data_process
 
 st.set_page_config(layout="wide")
 
 
-
-##페이지 상단 영역
+# 페이지 상단 영역
 st.subheader("PuzzleAI's 사업부 대시보드")
 
 col9, col10 = st.columns([8, 2])
 st.subheader("Notion DB를 기준으로 분석한 자료입니다.:sunglasses:")
 
 # 탭메뉴 영역
-tab1, tab2, tab3, tab4 = st.tabs(["VoiceENR", "VoiceSDK", "VoiceMARK","VoiceDOC"])
+tab1, tab2, tab3, tab4 = st.tabs(
+    ["VoiceENR", "VoiceSDK", "VoiceMARK", "VoiceDOC"])
 
 with tab1:
     col1, col2 = st.columns([5, 5])
-    st.write ("컨텐츠 삽입 예정")
+    st.markdown('통계자료는 아래와 같습니다.')
+
+    st.metric(label="정식계약", value=data_process.contract)
+    st.metric(label="데모계약", value=data_process.pre_contract)
