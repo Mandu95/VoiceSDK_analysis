b1bc0adecb24ef9b18912419436280ae2d387df8
Mandu95 <mandu95@puzzle-ai.com>
2024-05-25 15:30:47 +0900

    변경사항 저장 : 검색창 추가 탭 메뉴 선택박스 필터링으로 변경

--- pages/other_documents_management.py
+++ pages/other_documents_management.py
@@ -2,14 +2,10 @@ import streamlit as st
 from data_process import etc_document  # 데이터프레임을 data_process 모듈에서 불러옴
 
 # 표 높이와 너비 동적으로 설정하는 함수
-
-
 def get_table_dimensions():
     return 385, 2400  # 표의 높이와 너비를 설정
 
 # 데이터프레임을 페이징하는 함수
-
-
 def paginate_data(dataframe, page_number, items_per_page):
     """
     데이터프레임을 페이지 번호에 따라 분할하여 반환하는 함수.
@@ -23,8 +19,6 @@ def paginate_data(dataframe, page_number, items_per_page):
     return paged_df
 
 # 데이터 프레임을 표시하고 페이징하는 함수
-
-
 def display_paginated_table(dataframe, tab_label):
     """
     데이터프레임을 페이지 단위로 나누어 표시하는 함수.
@@ -62,34 +56,48 @@ def display_paginated_table(dataframe, tab_label):
     st.write(
         f"Displaying rows {(page_number - 1) * items_per_page + 1} to {min(page_number * items_per_page, total_items)} of {total_items}")
 
-
 def show_other_documents_management():
     st.subheader("기타 문서 관리")
     st.write("업체로 발송 된 견적서, MOU 및 NDA 체결 문서를 볼 수 있습니다:sunglasses:")
 
-    tabs = ["전체", "VoiceEMR", "VoiceENR", "VoiceSDK", "VoiceMARK", "VoiceDOC"]
-    selected_tab = st.tabs(tabs)
+    col1, col2 = st.columns([8, 2])
+    
+    with col1:
+        search_query = st.text_input(
+            "", 
+            value=st.session_state.get('search_query', ''),
+            placeholder="문서와 관련 된 업체 또는 병원 이름을 검색해주세요.", 
+            key='search_input'
+        )
+        st.session_state['search_query'] = search_query
 
-    for i, tab in enumerate(tabs):
-        with selected_tab[i]:
-            if tab == "전체":
-                filtered_data = etc_document
-            else:
-                filtered_data = etc_document[etc_document['문서이름'].str.contains(
-                    tab, case=False, na=False)]
+    with col2:
+        # 탭 메뉴를 selectbox로 대체
+        tab_label = st.selectbox(
+            "문서 구분",
+            ["전체", "VoiceEMR", "VoiceENR", "VoiceSDK", "VoiceMARK", "VoiceDOC"],
+            key='select_tab'
+        )
 
-            filtered_data = filtered_data.reset_index(drop=True)
-            filtered_data.index = filtered_data.index + 1
-            filtered_data.index.name = 'No'
+    if tab_label == "전체":
+        filtered_data = etc_document
+    else:
+        filtered_data = etc_document[etc_document['문서이름'].str.contains(tab_label, case=False, na=False)]
 
-            if filtered_data.empty:
-                st.markdown(
-                    """
-                    <div class="no-data">데이터가 없습니다</div>
-                    """, unsafe_allow_html=True)
-            else:
-                display_paginated_table(filtered_data, tab)
+    if search_query:
+        filtered_data = filtered_data[filtered_data['문서이름'].str.contains(search_query, case=False, na=False)]
 
+    filtered_data = filtered_data.reset_index(drop=True)
+    filtered_data.index = filtered_data.index + 1
+    filtered_data.index.name = 'No'
+
+    if filtered_data.empty:
+        st.markdown(
+            """
+            <div class="no-data">데이터가 없습니다</div>
+            """, unsafe_allow_html=True)
+    else:
+        display_paginated_table(filtered_data, tab_label)
 
 # 호출 예제
 if __name__ == "__main__":
