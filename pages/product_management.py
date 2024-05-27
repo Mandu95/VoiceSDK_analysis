b0e4a2f6c28bbfe0e6cce142f6346d919ad75aa7
mandu95@puzzle-ai.com <mandu95@puzzle-ai.com>
2024-05-27 11:02:35 +0900

    변경사항 저장 : 필요없는 행 안보이도록 수정

--- pages/product_management.py
+++ pages/product_management.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from data_process import product_management  # 데이터 로드
 
+
 def show_product_management():
-    df = product_management  # 데이터프레임 설정
+    df = product_management.drop(columns=['개발언어', '계약관리'])  # 필요한 열만 남기고 제거
 
     st.subheader("제품 현황 관리")
     st.write("Notion DB를 기준으로 분석한 자료이며, 오전 8시, 12시, 15시 하루 3회 동기화 됩니다.:sunglasses:")
@@ -19,7 +20,8 @@ def show_product_management():
         start_index = (page_number - 1) * items_per_page
         end_index = start_index + items_per_page
         paged_df = dataframe.iloc[start_index:end_index]
-        paged_df.index = range(start_index + 1, start_index + 1 + len(paged_df))
+        paged_df.index = range(
+            start_index + 1, start_index + 1 + len(paged_df))
         return paged_df
 
     # 세션 상태를 초기화하는 함수
@@ -44,7 +46,8 @@ def show_product_management():
     def display_paginated_table(dataframe, tab_label):
         init_session_state(tab_label)
         total_items = len(dataframe)
-        total_pages = max(1, (total_items + items_per_page - 1) // items_per_page)
+        total_pages = max(
+            1, (total_items + items_per_page - 1) // items_per_page)
 
         col1, col2 = st.columns([10, 1])
         with col2:
@@ -74,12 +77,13 @@ def show_product_management():
     def display_tab(dataframe, tab_label):
         init_session_state(tab_label)
         search_query = st.session_state['search_query']
-        
+
         col1, col2 = st.columns([8, 2])
 
         with col1:
             # 검색 기능 추가
-            search_query = st.text_input("", search_query, placeholder="업체 이름 또는 병원 이름을 입력해주세요", key='search_input')
+            search_query = st.text_input(
+                "", search_query, placeholder="업체 이름 또는 병원 이름을 입력해주세요", key='search_input')
             st.session_state['search_query'] = search_query
 
         with col2:
@@ -93,31 +97,37 @@ def show_product_management():
         if tab_label == "전체":
             filtered_data = dataframe
         else:
-            filtered_data = dataframe[dataframe['연관 제품'].apply(lambda x: tab_label in x if isinstance(x, list) else tab_label == x)]
+            filtered_data = dataframe[dataframe['연관 제품'].apply(
+                lambda x: tab_label in x if isinstance(x, list) else tab_label == x)]
 
         if search_query:
-            filtered_data = filtered_data[filtered_data['업체 이름'].str.contains(search_query, case=False, na=False)]
+            filtered_data = filtered_data[filtered_data['업체 이름'].str.contains(
+                search_query, case=False, na=False)]
 
         if filtered_data.empty:
-            st.markdown("<div class='no-data'>데이터가 없습니다</div>", unsafe_allow_html=True)
+            st.markdown("<div class='no-data'>데이터가 없습니다</div>",
+                        unsafe_allow_html=True)
         else:
             display_paginated_table(filtered_data, tab_label)
 
     # 세션 초기화
     init_session_state("전체")
-    
+
     # 데이터 표시
     tab_label = st.session_state['select_tab'] if 'select_tab' in st.session_state else "전체"
     if tab_label == "전체":
-        filtered_data = product_management
+        filtered_data = product_management.drop(columns=['개발언어', '계약관리'])
     else:
-        filtered_data = product_management[product_management['연관 제품'].apply(lambda x: tab_label in x if isinstance(x, list) else tab_label == x)]
+        filtered_data = product_management[product_management['연관 제품'].apply(
+            lambda x: tab_label in x if isinstance(x, list) else tab_label == x)]
+        filtered_data = filtered_data.drop(columns=['개발언어', '계약관리'])
 
     filtered_data = filtered_data.reset_index(drop=True)
     filtered_data.index = filtered_data.index + 1
     filtered_data.index.name = 'No'
 
     display_tab(filtered_data, tab_label)
+
 
 if __name__ == "__main__":
     show_product_management()
