d897501a9fdaf744c61129485ba965ae796ccf98
mandu95@puzzle-ai.com <mandu95@puzzle-ai.com>
2024-05-30 14:06:37 +0900

    변경사항 저장 : url 링크 이슈 해결

--- pages/contract_management.py
+++ pages/contract_management.py
@@ -9,10 +9,10 @@ def show_contract_management():
     st.write("정식/데모 계약서를 확인할 수 있습니다.:sunglasses:")
 
     # 필요 없는 열을 제거
-    df = contract_manage.drop(columns=['제품 현황 관리', '계약경로', '매입/매출'])
+    df = contract_manage.drop(columns=['제품', '제품 현황 관리', '계약경로', '매입/매출'])
 
     columns_order = ['계약명', '계약구분', '계약시작일', '라이선스 수', '계약단가',
-                     '라이선스 총액', '계약총액', '문서확인',]
+                     '라이선스 총액', '계약총액', '문서확인', '사본링크', '페이지URL']
     df_reordered = df.reindex(columns=columns_order)
 
     sf.display_tab(df_reordered, "계약서 관리", 10)
