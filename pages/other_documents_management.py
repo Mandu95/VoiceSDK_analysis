6c3e16a36112283bb833163f47a948e7dc86058f
mandu95@puzzle-ai.com <mandu95@puzzle-ai.com>
2024-05-30 14:18:51 +0900

    변경사항 저장 : 페이지 URL 에러 수정

--- pages/other_documents_management.py
+++ pages/other_documents_management.py
@@ -9,9 +9,12 @@ def show_other_documents_management():
     st.subheader("기타 문서 관리")
     st.write("업체로 발송 된 견적서, MOU 및 NDA 체결 문서를 볼 수 있습니다:sunglasses:")
 
+    df = etc_manage.drop(
+        columns=['발송 대상', '제품'])  # 필요한 열만 남기고 제거
+
     # 데이터프레임 열 순서 변경
     columns_order = ['문서이름', '라이선스 수', '계약단가',
-                     '라이선스 총액', '계약총액', '견적 유효 마감일', '문서확인',]
+                     '라이선스 총액', '계약총액', '견적 유효 마감일', '문서확인', '사본링크', '페이지URL']
     etc_manage_reordered = etc_manage.reindex(columns=columns_order)
 
     # sf.display_tab 함수 호출
