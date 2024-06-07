e0caec7b9d9c73fc47902ac95919290b85d908b1
mandu95@puzzle-ai.com <mandu95@puzzle-ai.com>
2024-06-07 14:20:34 +0900

    변경사항 저장 : 로그인 기능 이식

--- Home.py
+++ Home.py
@@ -1,103 +1,59 @@
 import streamlit as st
-import streamlit_function
-import real_data_analysis
-import threading
-import schedule
-from datetime import datetime
 import logging
-import time
+import streamlit_function as sf
+
+# 메인 콘텐츠 표시
 
 
-# 페이지 설정
-st.set_page_config(page_title="PuzzleAI's Dashboard", layout="wide")
+def main_content():
+    sf.load_css()
+    sf.set_initial_page()
 
-# 로그 설정
-logging.basicConfig(filename='data_sync.log',
-                    level=logging.INFO, format='%(asctime)s - %(message)s')
-
-# CSS 파일 로드 및 기본 제목과 메뉴 항목 숨기기
-
-
-def load_css():
-    with open("styles.css", "r", encoding="utf-8") as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-    st.markdown("""
-        <style>
-        .css-1d391kg, .css-1y4p8pa {visibility: hidden;}
-        </style>
-    """, unsafe_allow_html=True)
-
-# 초기 페이지 설정
-
-
-def set_initial_page():
-    # 페이지 레이아웃 설정
-    col_header, col_buttons = st.columns([8, 2])
-    with col_header:
-        st.header("Welcome to PuzzleAI's Dashboard")
-
-    # styles.css 파일 로드
-    with open("styles.css", "r", encoding="utf-8") as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-    with col_buttons:
-        st.markdown(
-            """
-            <div class="button-container">
-                <a href="https://www.notion.so/puzzleai/69aeff6ca32d4466ad4748dde3939e8b?v=3de75aac58cd42978178f02e0b3d7707" target="_blank">
-                    <button class="button notion-button">고객 관리</button>
-                </a>
-                <a href="https://puszleai-my.sharepoint.com/:f:/g/personal/mandu95_puzzle-ai_com/Egh0NiS6DdRPo8ej06sndswB7z9FOPB7OIAArnEenTObvw?e=igldVp" target="_blank">
-                    <button class="button onedrive-button">사업부 공유폴더</button>
-                </a>
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
-
-# 데이터 동기화를 위한 함수
-
-
-def main():
-    load_css()
-    set_initial_page()
+    import real_data_analysis
 
     # 탭 구성
-    tab_titles = ["VoiceEMR", "VoiceENR",
-                  "VoiceSDK", "VoiceMARK", "VoiceDOC"]
+    tab_titles = ["VoiceEMR", "VoiceENR", "VoiceSDK", "VoiceMARK", "VoiceDOC"]
     tabs = st.tabs(tab_titles)
 
     with tabs[0]:
-        # 여기 VoiceEMR 페이지에 보여줄 데이터를 추가하세요.
         notion_df, result = real_data_analysis.main("VoiceEMR")
-        streamlit_function.dashboard_button_df(
-            notion_df[0], "상태", result, "VoiceEMR")
+        sf.dashboard_button_df(notion_df[0], "상태", result, "VoiceEMR")
 
     with tabs[1]:
-        # 여기 VoiceEMR 페이지에 보여줄 데이터를 추가하세요.
         notion_df, result = real_data_analysis.main("VoiceENR")
-
-        streamlit_function.dashboard_button_df(
-            notion_df[0], "상태", result, "VoiceENR")
+        sf.dashboard_button_df(notion_df[0], "상태", result, "VoiceENR")
 
     with tabs[2]:
-        # 여기 VoiceEMR 페이지에 보여줄 데이터를 추가하세요.
         notion_df, result = real_data_analysis.main("VoiceSDK")
-        streamlit_function.dashboard_button_df(
-            notion_df[0], "상태", result, "VoiceSDK")
+        sf.dashboard_button_df(notion_df[0], "상태", result, "VoiceSDK")
 
     with tabs[3]:
-        # 여기 VoiceEMR 페이지에 보여줄 데이터를 추가하세요.
         notion_df, result = real_data_analysis.main("VoiceMARK")
-
-        streamlit_function.dashboard_button_df(
-            notion_df[0], "상태", result, "VoiceMARK")
+        sf.dashboard_button_df(notion_df[0], "상태", result, "VoiceMARK")
 
     with tabs[4]:
         st.markdown("제품 개발을 위한 협약 단계에 있습니다. 차후 데이터가 업로드 되면 표시됩니다.")
 
-        # streamlit_function.dashboard_button_df(notion_df[0],"상태",result,"VoiceDOC")
+# 메인 함수
+
+
+def main():
+    st.set_page_config(page_title="PuzzleAI's Dashboard", layout="wide")
+
+    logging.basicConfig(filename='data_sync.log',
+                        level=logging.INFO, format='%(asctime)s - %(message)s')
+
+    if 'logged_in' not in st.session_state:
+        st.session_state['logged_in'] = False
+        st.session_state['signup'] = False
+
+    if st.session_state['logged_in']:
+        main_content()
+    else:
+        if st.session_state['signup']:
+            sf.signup_screen()
+        else:
+            sf.login_screen()
 
 
 if __name__ == "__main__":
--- streamlit_function.py
+++ streamlit_function.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
+import streamlit as st
+import yaml
+import os
+import hashlib
 
 
 def get_table_dimensions():
@@ -358,3 +362,126 @@ def display_dataframe(df):
 
     # 데이터프레임 표시
     components.html(table_html, height=400, scrolling=True)
+
+
+# 사용자 데이터를 저장할 파일 경로
+USER_DATA_FILE = 'user_data.yaml'
+
+# 사용자 데이터를 로드하거나 파일이 없으면 초기화
+
+
+def load_user_data():
+    if os.path.exists(USER_DATA_FILE):
+        with open(USER_DATA_FILE, 'r') as file:
+            return yaml.safe_load(file)
+    else:
+        return {"users": {}}
+
+# 사용자 데이터를 저장
+
+
+def save_user_data(user_data):
+    with open(USER_DATA_FILE, 'w') as file:
+        yaml.dump(user_data, file)
+
+# 비밀번호를 해싱하는 함수
+
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+# 로그인 화면 표시
+
+
+def login_screen():
+    st.title("Login")
+
+    login_form = st.form("login_form")
+    email = login_form.text_input("Email")
+    password = login_form.text_input("Password", type="password")
+    login_button = login_form.form_submit_button("Login")
+
+    if login_button:
+        user_data = load_user_data()
+        hashed_password = hash_password(password)
+
+        if email in user_data["users"] and user_data["users"][email]["password"] == hashed_password:
+            st.session_state["logged_in"] = True
+            st.session_state["email"] = email
+            st.experimental_rerun()
+        else:
+            st.error("Invalid email or password")
+
+    st.button("Sign Up", on_click=lambda: st.session_state.update(
+        {"signup": True}))
+
+# 회원가입 화면 표시
+
+
+def signup_screen():
+    st.title("Sign Up")
+
+    signup_form = st.form("signup_form")
+    email = signup_form.text_input("Email")
+    username = signup_form.text_input("Username")
+    password = signup_form.text_input("Password", type="password")
+    signup_button = signup_form.form_submit_button("Sign Up")
+
+    if signup_button:
+        if "@puzzle-ai.com" not in email:
+            st.error("Email must contain @puzzle-ai.com")
+        else:
+            user_data = load_user_data()
+            if email in user_data["users"]:
+                st.error("Email already exists")
+            else:
+                user_data["users"][email] = {
+                    "username": username,
+                    "password": hash_password(password)
+                }
+                save_user_data(user_data)
+                st.success("Sign up successful! Please log in.")
+                st.session_state["signup"] = False
+                st.experimental_rerun()
+
+    st.button("Back to Login",
+              on_click=lambda: st.session_state.update({"signup": False}))
+
+# CSS 파일 로드 및 기본 제목과 메뉴 항목 숨기기
+
+
+def load_css():
+    with open("styles.css", "r", encoding="utf-8") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+    st.markdown("""
+        <style>
+        .css-1d391kg, .css-1y4p8pa {visibility: hidden;}
+        </style>
+    """, unsafe_allow_html=True)
+
+# 초기 페이지 설정
+
+
+def set_initial_page():
+    col_header, col_buttons = st.columns([8, 2])
+    with col_header:
+        st.header("Welcome to PuzzleAI's Dashboard")
+
+    with open("styles.css", "r", encoding="utf-8") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+    with col_buttons:
+        st.markdown(
+            """
+            <div class="button-container">
+                <a href="https://www.notion.so/puzzleai/69aeff6ca32d4466ad4748dde3939e8b?v=3de75aac58cd42978178f02e0b3d7707" target="_blank">
+                    <button class="button notion-button">고객 관리</button>
+                </a>
+                <a href="https://puszleai-my.sharepoint.com/:f:/g/personal/mandu95_puzzle-ai_com/Egh0NiS6DdRPo8ej06sndswB7z9FOPB7OIAArnEenTObvw?e=igldVp" target="_blank">
+                    <button class="button onedrive-button">사업부 공유폴더</button>
+                </a>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
