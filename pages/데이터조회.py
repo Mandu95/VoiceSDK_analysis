bde9a1f902981f2cf8d50881edd780313ee71bf3
Mandu95 <mandu95@puzzle-ai.com>
2024-06-15 11:31:05 +0900

    변경사항 저장 : Ready_notion_DB.py 리펙토링에 맞춰서 코드 일부 수정

--- pages/데이터조회.py
+++ pages/데이터조회.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import streamlit_function as sf  # streamlit_function 모듈 임포트
 import login_function as lf  # 로그인 모듈 임포트
+import Home
 
 st.set_page_config(page_title="데이터 조회", layout="wide")
 
@@ -11,7 +12,6 @@ if 'logged_in' not in st.session_state or not st.session_state['logged_in']:
     st.stop()
 
 else:
-    from Ready_notion_DB import product_manage, etc_manage, contract_manage  # 데이터 로드
 
     # 로그아웃 버튼 추가
     lf.add_logout_button()
@@ -21,54 +21,60 @@ else:
     st.write("업체 상태, 계약서, 견적서 등 데이터를 직접 조회합니다.:sunglasses:")
 
     def show_contract_management():
+        # 세션 상태에 로딩된 데이터를 저장
+        contract_manage = st.session_state['contract_manage']
         st.write("정식/데모 계약서를 확인할 수 있습니다.:sunglasses:")
 
         # 필요 없는 열을 제거
-        contract_manage_df = contract_manage.drop(
+        contract_manage = contract_manage.drop(
             columns=['제품 현황 관리', '계약경로', '매입/매출'])
 
         columns_order = ['계약명', '계약구분', '계약시작일', '라이선스 수', '계약단가', '제품',
                          '라이선스 총액', '계약총액', '사본링크', '페이지URL']
-        contract_manage_df_reordered = contract_manage_df.reindex(
+        contract_manage = contract_manage.reindex(
             columns=columns_order)
 
-        contract_manage_df_reordered = sf.URL_insert(
-            contract_manage_df_reordered)
-        sf.display_dataframe(contract_manage_df_reordered, "계약서 관리")
+        contract_manage = sf.URL_insert(
+            contract_manage)
+        sf.display_dataframe(contract_manage, "계약서 관리")
 
     # 기타 문서 관리 페이지 함수
     def show_other_documents_management():
+        # 세션 상태에 로딩된 데이터를 저장
+        etc_manage = st.session_state['etc_manage'] 
         st.write("업체로 발송 된 견적서, MOU 및 NDA 체결 문서를 볼 수 있습니다:sunglasses:")
 
-        etc_manage_df = etc_manage.drop(
+        etc_manage = etc_manage.drop(
             columns=['발송 대상'])  # 필요한 열만 남기고 제거
 
         # 데이터프레임 열 순서 변경
         columns_order = ['문서이름', '제품', '라이선스 수', '계약단가',
                          '라이선스 총액', '계약총액', '견적 유효 마감일', '사본링크', '페이지URL']
-        etc_manage_df_reordered = etc_manage_df.reindex(columns=columns_order)
+        etc_manage = etc_manage.reindex(columns=columns_order)
 
         # sf.display_tab 함수 호출
         # sf.display_tab(etc_manage_reordered, "기타 문서 관리", 10)
-        etc_manage_df_reordered = sf.URL_insert(etc_manage_df_reordered)
-        sf.display_dataframe(etc_manage_df_reordered, "기타 문서 관리")
+        etc_manage = sf.URL_insert(etc_manage)
+        sf.display_dataframe(etc_manage, "기타 문서 관리")
 
     # 제품 현황 관리 페이지 함수
     def show_product_management():
-        product_manage_df = product_manage.drop(
+        # 세션 상태에 로딩된 데이터를 저장
+        cop_manage = st.session_state['product_manage']
+        cop_manage = cop_manage.drop(
             columns=['개발언어', '납품병원', '계약구분', '계약 횟수', '계약관리', '라이선스 수', '기타문서 (견적서, NDA 등)'])  # 필요한 열만 남기고 제거
 
         # 열 순서 변경
         columns_order = ['업체 이름', '상태', '담당자 이메일', '제품',
                          '컨택 업체 담당자', '계약시작일', '계약종료일', '계약잔여일', '정보 최신화 날짜', '페이지URL']
-        product_manage_df_reordered = product_manage_df.reindex(
+        product_manage_df_reordered = cop_manage.reindex(
             columns=columns_order)
 
         st.write("Notion DB를 기준으로 분석한 자료이며, 오전 7시, 12시 하루 2회 동기화 됩니다.:sunglasses:")
 
-        product_manage_df_reordered = sf.URL_insert(
-            product_manage_df_reordered)
-        sf.display_dataframe(product_manage_df_reordered, "제품 현황 관리")
+        cop_manage = sf.URL_insert(
+            cop_manage)
+        sf.display_dataframe(cop_manage, "제품 현황 관리")
 
     # 탭 구성
     tab1, tab2, tab3 = st.tabs(["업체추적", "계약서", "기타서류"])
--- pages/업무.py
+++ pages/업무.py
@@ -12,27 +12,27 @@ if 'logged_in' not in st.session_state or not st.session_state['logged_in']:
 
 else:
 
-    from Ready_notion_DB import Task  # 데이터 로드
-
     # 로그아웃 버튼 추가
     lf.add_logout_button()
 
     def show_product_management():
+        # 세션 상태에 로딩된 데이터를 저장
+        Task = st.session_state['Task'] 
 
         # 'age' 열을 기준으로 내림차순 정렬
-        df = Task.sort_values(by='최종 편집 일시', ascending=False)
-        df = Task.drop(
+        Task = Task.sort_values(by='최종 편집 일시', ascending=False)
+        Task = Task.drop(
             columns=['요청자', '담당부서', '사업부 달력 (관련 일정)'])  # 필요한 열만 남기고 제거
 
         # 열 순서 변경
         columns_order = ['업무', '분류', '상태', '담당자',
-                         '업무기간', '우선순위', '페이지URL', '관련 문서']
-        df = df.reindex(columns=columns_order)
+                         '업무기간', '우선순위', '페이지URL']
+        Task = Task.reindex(columns=columns_order)
 
         st.subheader("업체 별 업무 진행상황 관리")
         st.write("Notion DB를 기준으로 분석한 자료입니다.:sunglasses:")
 
-        sf.display_dataframe(df, "업무")
+        sf.display_dataframe(Task, "업무")
 
     if __name__ == "__main__":
         show_product_management()
